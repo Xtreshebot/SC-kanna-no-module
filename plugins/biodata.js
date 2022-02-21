5f2ee15c23f80f1f5daacf9b575e9a8dd1185a10
xtreshebot <bangkitwiguno5@gmail.com>
2022-02-21 19:10:12 +0700

    first commit

---
+++ plugins/biodata.js
@@ -0,0 +1,64 @@
+const { MessageType } = require('@adiwajshing/baileys')
+let fetch = require('node-fetch')
+let fs = require('fs')
+let handler = async (m, { conn, text }) => {
+let logo = global.logoowner
+  let ext= `
+*───────[ BIODATA OWNER ]───────*
+*💌 Nama* : hans
+*🎨 Umur* : 16
+*🧮 Kelas* : 11
+*📈 Status* : rahasia lah🗿
+
+*───────[ SOSIAL MEDIA ]───────*
+*📷 instagran* :
+Kepo🗿
+*🇫  Facebook* : kepho
+*🏮 Chanel Youtube* : kepo amat🗿
+`
+let name = await conn.getName(m.sender)
+
+let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+
+  sumberImg = fs.readFileSync(`./src/owner.jpg`)
+  image = (await conn.prepareMessage('0@s.whatsapp.net', sumberImg, MessageType.image, { thumbnail: Buffer.alloc(0) })).message.imageMessage
+  res = await conn.prepareMessageFromContent(m.chat, {
+    "productMessage": {
+      "product": {
+        "productImage": image,
+        "productId": "4938174216214248",
+        "title": "───────[ OWNER ]───────",
+        "description": '\n\n' + ext,
+        "retailerId": "Itu Owner ku ya ^~^",
+        "url": '',
+        "descriptionCount": "999999999",
+        "productImageCount": "1",
+      },
+      "businessOwnerJid": "6285742371264@s.whatsapp.net",
+      "contextInfo": {
+        "forwardingScore": 9999,
+        "isForwarded": false
+      }
+    }
+  },
+    { quoted: fkon })
+  conn.relayWAMessage(res)
+
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+handler.command = /^(owner|creator)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+let wm = global.botwm
